5bdc798221d9cefdd46f4b145e82326a87e258bc
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2023-02-24 12:22:23 +0800

    Remove Eugene Badato

--- dev-data/manual_modify_data.js
+++ dev-data/manual_modify_data.js
@@ -4,12 +4,13 @@
  * sample: node dev-data/manual_modify_data.js --emptyImageAllItems
  */
 
-const mongoose = require('mongoose');
-require('dotenv').config();
-const Item = require('../models/item.model.js');
-const Order = require('../models/order.model.js');
+const mongoose = require('mongoose')
+require('dotenv').config()
+const Item = require('../models/item.model.js')
+const Order = require('../models/order.model.js')
+const Image = require('../models/image.model.js')
 
-console.log(process.env.MONGODB_URI);
+console.log(process.env.MONGODB_URI)
 
 // !this code is to remove a field in document
 // !It will permanently deleted the field in every document
@@ -26,89 +27,105 @@ console.log(process.env.MONGODB_URI);
 
 const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGODB_URI);
+		const conn = await mongoose.connect(process.env.MONGODB_URI)
 
-		console.log(`MongoDB Connected: ${conn.connection.host}`);
+		console.log(`MongoDB Connected: ${conn.connection.host}`)
 	} catch (error) {
-		console.log(error);
-		process.exit(1);
+		console.log(error)
+		process.exit(1)
 	}
-};
+}
 
-connectDB();
+connectDB()
 
 const emptyImageAllItems = async () => {
 	try {
-		const res = await Item.updateMany({}, { $unset: { coverPhoto: '' } });
-		console.log(res, 'Succesfully Empty Image All Data!');
-		process.exit();
+		const res = await Item.updateMany({}, { $unset: { coverPhoto: '' } })
+		console.log(res, 'Succesfully Empty Image All Data!')
+		process.exit()
 	} catch (error) {
-		console.log(error);
+		console.log(error)
 	}
-};
+}
 
 const unPublishedAllItems = async () => {
 	try {
-		await Item.updateMany({}, { $set: { isPublished: 0 } });
-		console.log('Succesfully Unublished All Data!');
-		process.exit();
+		await Item.updateMany({}, { $set: { isPublished: 0 } })
+		console.log('Succesfully Unublished All Data!')
+		process.exit()
 	} catch (error) {
-		console.log(error);
+		console.log(error)
 	}
-};
+}
 
 const pendingAllOrders = async () => {
 	try {
-		await Order.updateMany({}, { $set: { status: 'pending' } });
-		console.log('Succesfully Pending All Orders!');
-		process.exit();
+		await Order.updateMany({}, { $set: { status: 'pending' } })
+		console.log('Succesfully Pending All Orders!')
+		process.exit()
 	} catch (error) {
-		console.log(error);
+		console.log(error)
 	}
-};
+}
 
 const publishedAllItems = async () => {
 	try {
-		await Item.updateMany({}, { $set: { isPublished: 1 } });
-		console.log('Succesfully Published All Data!');
-		process.exit();
+		await Item.updateMany({}, { $set: { isPublished: 1 } })
+		console.log('Succesfully Published All Data!')
+		process.exit()
 	} catch (error) {
-		console.log(error);
+		console.log(error)
 	}
-};
+}
 
 const deleteOrderData = async () => {
 	try {
-		await Order.deleteMany();
+		await Order.deleteMany()
 		//   await User.deleteMany();
 		//   await Review.deleteMany();
-		console.log('Data successfully deleted!');
+		console.log('Data successfully deleted!')
 	} catch (err) {
-		console.log(err);
+		console.log(err)
 	}
-	process.exit();
-};
+	process.exit()
+}
 
-console.log(process.argv);
+const deleteAllImageData = async () => {
+	try {
+		await Image.deleteMany()
+		//   await User.deleteMany();
+		//   await Review.deleteMany();
+		console.log('Data successfully deleted!')
+	} catch (err) {
+		console.log(err)
+	}
+	process.exit()
+}
+
+console.log(process.argv)
 
 if (process.argv[2] === '--unPublishedAllItems') {
-	unPublishedAllItems();
+	unPublishedAllItems()
 }
 
 if (process.argv[2] === '--publishedAllItems') {
-	publishedAllItems();
+	publishedAllItems()
 }
 
 if (process.argv[2] === '--deleteAllOrders') {
-	deleteOrderData();
+	deleteOrderData()
 }
 
 if (process.argv[2] === '--pendingAllOrders') {
-	pendingAllOrders();
+	pendingAllOrders()
 }
 
 if (process.argv[2] === '--emptyImageAllItems') {
-	emptyImageAllItems();
+	emptyImageAllItems()
+}
+
+if (process.argv[2] === '--deleteAllImageData') {
+	deleteAllImageData()
 }
 
 // mongoose
